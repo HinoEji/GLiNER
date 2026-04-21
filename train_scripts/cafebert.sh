3d1d1c8906d415adefcea46fd8676c1f9d19bab3
NguyenHongSon <sonnguyen.15012004@gmail.com>
2026-04-21 18:10:40 +0000

    fix sh

--- train_scripts/cafebert.sh
+++ train_scripts/cafebert.sh
@@ -1,4 +1,4 @@
-python train.py --config train_config/cafebert_config.yaml --path output/cafebert
+python train.py --config train_config/cafebert.yaml --path output/cafebert
 python eval.py --model "output/cafebert" --batch_size 16 --log_dir "output/cafebert/predict" --threshold 0.5
 python eval.py --model "output/cafebert" --batch_size 16 --log_dir "output/cafebert/predict" --threshold 0.6
 python eval.py --model "output/cafebert" --batch_size 16 --log_dir "output/cafebert/predict" --threshold 0.7
